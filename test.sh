0a72634405b6757c5967c84252efea20e8ccb8c5
Nobuhiro Oto <nobuhirooto@gmail.com>
2025-05-21 01:39:08 +0900

    Added tests for equality

--- test.sh
+++ test.sh
@@ -47,18 +47,24 @@ assert 15 "-3 * (-4 + -1)"
 assert 55 "  1 +  2+ 3 +4+5+   6 + 7   +8+9  +  10"
 assert 100 "  (   5   +   5  )  *  (   5  +   5   ) "
 
+# Tests for equality
+assert 0 "0==1"
+assert 1 "42==42"
+assert 1 "0!=1"
+assert 0 "42!=42"
+
 # Tests for comparators
-assert 1 '0<1'
-assert 0 '1<1'
-assert 0 '2<1'
-assert 1 '0<=1'
-assert 1 '1<=1'
-assert 0 '2<=1'
-assert 1 '1>0'
-assert 0 '1>1'
-assert 0 '1>2'
-assert 1 '1>=0'
-assert 1 '1>=1'
-assert 0 '1>=2'
+assert 1 "0<1"
+assert 0 "1<1"
+assert 0 "2<1"
+assert 1 "0<=1"
+assert 1 "1<=1"
+assert 0 "2<=1"
+assert 1 "1>0"
+assert 0 "1>1"
+assert 0 "1>2"
+assert 1 "1>=0"
+assert 1 "1>=1"
+assert 0 "1>=2"
 
 echo "All tests succeeded 🎉"
