03dc4fc4e6dd9d8e8ae55fe7b6e9bea1a0fdfcdf
Nobuhiro Oto <nobuhirooto@gmail.com>
2025-07-30 05:08:36 +0900

    functions

--- codegen.c
+++ codegen.c
@@ -4,7 +4,7 @@ static int depth;
 static int label_count = 0;
 
 static void push(void){
-  printf("  push %%rax\n");
+  printf("  push rax\n");
   depth++;
 }
 static void pop(char *arg) {
@@ -14,7 +14,7 @@ static void pop(char *arg) {
 
 static void gen_addr(Node* node){
   if(node->kind == ND_LVAR){
-    printf("  lea %d(%%rbp), %%rax\n", -node->offset);
+    printf("  lea rax, [rbp%+d]\n", -node->offset);
     return;
   }
   error("not an lvalue");
@@ -23,59 +23,76 @@ static void gen_addr(Node* node){
 static void gen_expr(Node *node) {
   switch (node->kind) {
   case ND_NUM:
-    printf("  mov $%d, %%rax\n", node->val);
+    printf("  mov rax, %d\n", node->val);
     return;
   case ND_NEG:
     gen_expr(node->lhs);
-    printf("  neg %%rax\n");
+    printf("  neg rax\n");
     return;
   case ND_LVAR:
     gen_addr(node);
-    printf("  mov (%%rax), %%rax\n");
+    printf("  mov rax, [rax]\n");
     return;
   case ND_ASSIGN:
     gen_addr(node->lhs);
     push();
     gen_expr(node->rhs);
-    pop("%rdi");
-    printf("  mov %%rax, (%%rdi)\n");
+    pop("rdi");
+    printf("  mov [rdi], rax\n");
     return;
+  case ND_FUNCALL: {
+    int arg_count = 0;
+    char *regs[] = {"rdi", "rsi", "rdx", "rcx", "r8", "r9"};
+    
+    for (Node *arg = node->args; arg; arg = arg->next) {
+      gen_expr(arg);
+      push();
+      arg_count++;
+    }
+    
+    for (int i = arg_count - 1; i >= 0; i--) {
+      pop(regs[i]);
+    }
+    
+    printf("  call %s\n", node->funcname);
+    return;
+  }
   }
   gen_expr(node->rhs);
   push();
   gen_expr(node->lhs);
-  pop("%rdi");
+  pop("rdi");
 
   switch (node->kind) {
   case ND_ADD:
-    printf("  add %%rdi, %%rax\n");
+    printf("  add rax, rdi\n");
     return;
   case ND_SUB:
-    printf("  sub %%rdi, %%rax\n");
+    printf("  sub rax, rdi\n");
     return;
   case ND_MUL:
-    printf("  imul %%rdi, %%rax\n");
+    printf("  imul rax, rdi\n");
     return;
   case ND_DIV:
     printf("  cqo\n");
-    printf("  idiv %%rdi\n");
+    printf("  idiv rdi\n");
     return;
   case ND_EQ:
   case ND_NE:
   case ND_LT:
   case ND_LE:
-    printf("  cmp %%rdi, %%rax\n");
+    printf("  cmp rax, rdi\n");
 
     if (node->kind == ND_EQ)
-      printf("  sete %%al\n");
+      printf("  sete al\n");
     else if (node->kind == ND_NE)
-      printf("  setne %%al\n");
+      printf("  setne al\n");
     else if (node->kind == ND_LT)
-      printf("  setl %%al\n");
+      printf("  setl al\n");
     else if (node->kind == ND_LE)
-      printf("  setle %%al\n");
+      printf("  setle al\n");
 
-    printf("  movzb %%al, %%rax\n");
+    printf("  movzx rax, al\n");
     return;
   }
 
@@ -94,16 +111,50 @@ void gen_stmt(Node* node){
 
   if (node->kind == ND_RETURN) {
     gen_expr(node->lhs);
-    printf("  mov %%rbp, %%rsp\n");
-    printf("  pop %%rbp\n");
+    printf("  mov rsp, rbp\n");
+    printf("  pop rbp\n");
     printf("  ret\n");
+    return;
+  }
+
+  if (node->kind == ND_FUNCTION) {
+    printf("  .globl %s\n", node->funcname);
+    printf("%s:\n", node->funcname);
+    printf("  push rbp\n");
+    printf("  mov rbp, rsp\n");
+    
+    int stack_size = 0;
+    for (LVar *lvar = node->locals; lvar; lvar = lvar->next) {
+      if (lvar->offset > stack_size)
+        stack_size = lvar->offset;
+    }
+    
+    // Ensure 16-byte stack alignment
+    if (stack_size == 0) {
+      stack_size = 16; // Minimum for alignment
+    } else {
+      stack_size = (stack_size + 15) / 16 * 16;
+    }
+    printf("  sub rsp, %d\n", stack_size);
+    
+    char *regs[] = {"rdi", "rsi", "rdx", "rcx", "r8", "r9"};
+    int param_count = 0;
+    for (Node *param = node->params; param; param = param->next) {
+      printf("  mov [rbp%+d], %s\n", -param->offset, regs[param_count]);
+      param_count++;
+    }
+    
+    for (Node *stmt = node->body; stmt; stmt = stmt->next) {
+      gen_stmt(stmt);
+      assert(depth == 0);
+    }
     return;
   }
 
   if (node->kind == ND_IF) {
     int label_num = label_count++;
     gen_expr(node->cond);
-    printf("  cmp $0, %%rax\n");
+    printf("  cmp rax, 0\n");
     if (node->els) {
       printf("  je .Lelse%d\n", label_num);
       gen_stmt(node->then);
@@ -123,7 +174,7 @@ void gen_stmt(Node* node){
     int label_num = label_count++;
     printf(".Lbegin%d:\n", label_num);
     gen_expr(node->cond);
-    printf("  cmp $0, %%rax\n");
+    printf("  cmp rax, 0\n");
     printf("  je .Lend%d\n", label_num);
     gen_stmt(node->body);
     printf("  jmp .Lbegin%d\n", label_num);
@@ -139,7 +190,7 @@ void gen_stmt(Node* node){
     printf(".Lbegin%d:\n", label_num);
     if (node->cond) {
       gen_expr(node->cond);
-      printf("  cmp $0, %%rax\n");
+      printf("  cmp rax, 0\n");
       printf("  je .Lend%d\n", label_num);
     }
     gen_stmt(node->body);
@@ -155,29 +206,9 @@ void gen_stmt(Node* node){
 }
 
 void codegen(Node *node) {
-  printf("  .globl main\n");
-  printf("main:\n");
-
-  printf("  push %%rbp\n");
-  printf("  mov %%rsp, %%rbp\n");
+  printf(".intel_syntax noprefix\n");
   
-  // Calculate stack size based on local variables
-  int stack_size = 0;
-  for (LVar *lvar = locals; lvar; lvar = lvar->next) {
-    if (lvar->offset > stack_size)
-      stack_size = lvar->offset;
-  }
-  // Align to 16 bytes
-  stack_size = (stack_size + 15) / 16 * 16;
-  
-  if (stack_size > 0)
-    printf("  sub $%d, %%rsp\n", stack_size);
-
   for(Node *n = node; n; n = n->next){
     gen_stmt(n);
-    assert(depth==0);
   }
-  printf("  mov %%rbp, %%rsp\n");
-  printf("  pop %%rbp\n");
-  printf("  ret\n");
 }
